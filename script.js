1f4e32a64ffd4886d19f2994c47c0727ad9b69e0
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-17 19:57:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var todo = ["item 1", "item 2", "item 3"] // Arrays are used to store multiple values in a single variable
+let todos = ["item 1", "item 2", "item 3"] // Arrays are used to store multiple values in a single variable
+
+Name("Ben");
+
+function Name(Name = 'default') {
+  console.log(`Is your name ${Name}?`);
+}
