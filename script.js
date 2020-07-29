dad0883b103f6d76d8846c5102fd2232e047132b
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-29 15:36:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-const todos = ['item 1', 'item 2', 'item 3', 'item 4'] // Arrays are used to store multiple values in a single variable
-
-function displayTodos() {
-  console.log('My todos:', todos);
-}
-
-displayTodos()
-
-function addTodo(todo) {
-  todos.push(todo);
-}
-
-addTodo('new item');
-
-function changeTodos(position, newValue) {
-  todos[position] = newValue;
-  console.log(displayTodos);
-}
-
-changeTodos(0, 'change 1');
-
-function deleteTodo() {
-  todos.splice(0, 1);
-}
-
-
-const myName = 'Ben';
-
-function sayName() {
-  console.log(myName);
-}
-sayName();
+/*const todos = ['item 1', 'item 2', 'item 3', 'item 4']; // Arrays are used to store multiple values in a single variable
+*/
+const todoList = {
+  todos: ['item 1', 'item 2', 'item 3', 'item 4']
+};
