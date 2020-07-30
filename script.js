38ac3cee4dec1a0889840ba1d908e69add2fc951
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-30 13:29:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 /*const todos = ['item 1', 'item 2', 'item 3', 'item 4']; // Arrays are used to store multiple values in a single variable
 */
 const todoList = {
-  todos: ['item 1', 'item 2', 'item 3', 'item 4']
+  todos: ['item 1', 'item 2', 'item 3', 'item 4'],
+  displayTodos: function () {
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function (todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  }
 };
